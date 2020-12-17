a711af615af9e4a7c2f2c48fde44305722db9faa
Margarita Nedzelska <margarita.nedzelska@sonarsource.com>
2020-12-17 22:22:22 +0100

    Add Test Reports Parser module

---
+++ test-reports-parser/src/test/kotlin/org/sonar/reports/data/UnitTestResultTest.kt
@@ -0,0 +1,31 @@
+package org.sonar.reports.data
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class UnitTestResultTest {
+
+    @Test
+    fun shouldBeError() {
+        val result = UnitTestResult(status = UnitTestResult.STATUS_ERROR)
+        assertThat(result.status).isEqualTo(UnitTestResult.STATUS_ERROR)
+        assertThat(result.isError()).isTrue
+        assertThat(result.isErrorOrFailure()).isTrue
+    }
+
+    @Test
+    fun shouldBeFailure() {
+        val result = UnitTestResult(status = UnitTestResult.STATUS_FAILURE)
+        assertThat(result.status).isEqualTo(UnitTestResult.STATUS_FAILURE)
+        assertThat(result.isError()).isFalse
+        assertThat(result.isErrorOrFailure()).isTrue
+    }
+
+    @Test
+    fun shouldBeSuccess() {
+        val result = UnitTestResult(status = UnitTestResult.STATUS_OK)
+        assertThat(result.status).isEqualTo(UnitTestResult.STATUS_OK)
+        assertThat(result.isError()).isFalse
+        assertThat(result.isErrorOrFailure()).isFalse
+    }
+}
