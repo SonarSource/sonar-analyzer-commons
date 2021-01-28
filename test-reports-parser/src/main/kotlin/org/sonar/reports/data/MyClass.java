b44d3abeab0c5461d75107b94e89ab9c85f2a10f
Margarita <margarita.nedzelska@sonarsource.com>
2021-01-28 16:33:13 +0100

    Try add java test and java code

---
+++ test-reports-parser/src/main/kotlin/org/sonar/reports/data/MyClass.java
@@ -0,0 +1,8 @@
+package org.sonar.reports.data;
+
+public class MyClass {
+  
+  public String helloWorld() {
+    return "Hello, World!";
+  }
+}
---
+++ test-reports-parser/src/test/kotlin/org/sonar/reports/data/MyTest.java
@@ -0,0 +1,13 @@
+package org.sonar.reports.data;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class MyTest {
+  
+  @Test
+  void test() {
+    Assert.assertEquals("Hello, World!", new MyClass().helloWorld());
+  }
+  
+}
