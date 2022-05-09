e61af1b82415480158c5c3919bc0785c34387082
quentin-jaquier-sonarsource <quentin.jaquier@sonarsource.com>
2022-05-09 15:37:32 +0200

    Add "MatchType" to improve precision of finders, Fix #221 (#224)

--- regex-parsing/src/main/java/org/sonarsource/analyzer/commons/regex/finders/ReluctantQuantifierWithEmptyContinuationFinder.java
+++ regex-parsing/src/main/java/org/sonarsource/analyzer/commons/regex/finders/ReluctantQuantifierWithEmptyContinuationFinder.java
@@ -20,6 +20,7 @@
 package org.sonarsource.analyzer.commons.regex.finders;
 
 import java.util.Collections;
+import org.sonarsource.analyzer.commons.regex.MatchType;
 import org.sonarsource.analyzer.commons.regex.RegexIssueReporter;
 import org.sonarsource.analyzer.commons.regex.RegexParseResult;
 import org.sonarsource.analyzer.commons.regex.ast.AutomatonState;
@@ -30,16 +31,24 @@ import org.sonarsource.analyzer.commons.regex.ast.StartState;
 import org.sonarsource.analyzer.commons.regex.helpers.RegexReachabilityChecker;
 import org.sonarsource.analyzer.commons.regex.helpers.RegexTreeHelper;
 
+import static org.sonarsource.analyzer.commons.regex.MatchType.NOT_SUPPORTED;
+
 public class ReluctantQuantifierWithEmptyContinuationFinder extends RegexBaseVisitor {
 
   private static final String MESSAGE_FIX = "Fix this reluctant quantifier that will only ever match %s repetition%s.";
   private static final String MESSAGE_UNNECESSARY = "Remove the '?' from this unnecessarily reluctant quantifier.";
 
   private final RegexIssueReporter.ElementIssue regexElementIssueReporter;
+  private final MatchType matchType;
   private AutomatonState endState;
 
   public ReluctantQuantifierWithEmptyContinuationFinder(RegexIssueReporter.ElementIssue regexElementIssueReporter) {
+    this(regexElementIssueReporter, NOT_SUPPORTED);
+  }
+
+  public ReluctantQuantifierWithEmptyContinuationFinder(RegexIssueReporter.ElementIssue regexElementIssueReporter, MatchType matchType) {
     this.regexElementIssueReporter = regexElementIssueReporter;
+    this.matchType = matchType;
   }
 
   @Override
@@ -51,14 +60,28 @@ public class ReluctantQuantifierWithEmptyContinuationFinder extends RegexBaseVis
   public void visitRepetition(RepetitionTree tree) {
     super.visitRepetition(tree);
     if (tree.getQuantifier().getModifier() == Quantifier.Modifier.RELUCTANT) {
-      if (RegexTreeHelper.isAnchoredAtEnd(tree.continuation())) {
+      if (isFullMatchOrAnchoredAtEnd(tree.continuation())) {
         if (RegexTreeHelper.onlyMatchesEmptySuffix(tree.continuation())) {
           regexElementIssueReporter.report(tree, MESSAGE_UNNECESSARY, null, Collections.emptyList());
         }
-      } else if (RegexReachabilityChecker.canReachWithoutConsumingInput(new StartState(tree.continuation(), tree.activeFlags()), endState)) {
+      } else if (canBePartialMatchOrNotSupported() &&
+        RegexReachabilityChecker.canReachWithoutConsumingInput(new StartState(tree.continuation(), tree.activeFlags()), endState)) {
         int minimumRepetitions = tree.getQuantifier().getMinimumRepetitions();
         regexElementIssueReporter.report(tree, String.format(MESSAGE_FIX, minimumRepetitions, minimumRepetitions == 1 ? "" : "s"), null, Collections.emptyList());
       }
     }
+  }
+
+  private boolean isFullMatchOrAnchoredAtEnd(AutomatonState state) {
+    return matchType == MatchType.FULL || RegexTreeHelper.isAnchoredAtEnd(state);
+  }
+
+  private boolean canBePartialMatchOrNotSupported() {
+    if (matchType == NOT_SUPPORTED) {
+      // If the match type is not set explicitly, continue anyway, taking a small risk to report erroneous issues.
+      return true;
+    }
+    // If we explicitly set the match type, we only want to continue if we are explicitly in PARTIAL mode, in order to avoid FP.
+    return matchType == MatchType.PARTIAL;
   }
 }
--- regex-parsing/src/test/java/org/sonarsource/analyzer/commons/regex/finders/ReluctantQuantifierWithEmptyContinuationFinderTest.java
+++ regex-parsing/src/test/java/org/sonarsource/analyzer/commons/regex/finders/ReluctantQuantifierWithEmptyContinuationFinderTest.java
@@ -20,6 +20,7 @@
 package org.sonarsource.analyzer.commons.regex.finders;
 
 import org.junit.jupiter.api.Test;
+import org.sonarsource.analyzer.commons.regex.MatchType;
 import org.sonarsource.analyzer.commons.regex.RegexIssueReporter;
 import org.sonarsource.analyzer.commons.regex.RegexParseResult;
 
@@ -30,10 +31,44 @@ class ReluctantQuantifierWithEmptyContinuationFinderTest {
     Verifier.verify(new ReluctantQuantifierWithEmptyContinuationFinderCheck(), "ReluctantQuantifierWithEmptyContinuationFinder.yml");
   }
 
+  @Test
+  void test_partial_match() {
+    // When the MatchType is explicitly PARTIAL, the behavior is the same as when the Type is not set at all.
+    Verifier.verify(new ReluctantQuantifierWithEmptyContinuationFinderExplicitMatchTypeCheck(MatchType.PARTIAL), "ReluctantQuantifierWithEmptyContinuationFinder.yml");
+  }
+
+  @Test
+  void test_full_match() {
+    Verifier.verify(new ReluctantQuantifierWithEmptyContinuationFinderExplicitMatchTypeCheck(MatchType.FULL), "ReluctantQuantifierWithEmptyContinuationFinderFullMatch.yml");
+  }
+
+  @Test
+  void test_both_match() {
+    Verifier.verify(new ReluctantQuantifierWithEmptyContinuationFinderExplicitMatchTypeCheck(MatchType.BOTH), "ReluctantQuantifierWithEmptyContinuationFinderBothOrUnknownMatch.yml");
+  }
+
+  @Test
+  void test_unknown_match() {
+    Verifier.verify(new ReluctantQuantifierWithEmptyContinuationFinderExplicitMatchTypeCheck(MatchType.UNKNOWN), "ReluctantQuantifierWithEmptyContinuationFinderBothOrUnknownMatch.yml");
+  }
+
   static class ReluctantQuantifierWithEmptyContinuationFinderCheck extends FinderCheck {
     @Override
     public void checkRegex(RegexParseResult parseResult, RegexIssueReporter.ElementIssue regexElementIssueReporter, RegexIssueReporter.InvocationIssue invocationIssueReporter) {
       new ReluctantQuantifierWithEmptyContinuationFinder(regexElementIssueReporter).visit(parseResult);
+    }
+  }
+
+  static class ReluctantQuantifierWithEmptyContinuationFinderExplicitMatchTypeCheck extends FinderCheck {
+    private final MatchType matchType;
+
+    ReluctantQuantifierWithEmptyContinuationFinderExplicitMatchTypeCheck(MatchType matchType) {
+      this.matchType = matchType;
+    }
+
+    @Override
+    public void checkRegex(RegexParseResult parseResult, RegexIssueReporter.ElementIssue regexElementIssueReporter, RegexIssueReporter.InvocationIssue invocationIssueReporter) {
+      new ReluctantQuantifierWithEmptyContinuationFinder(regexElementIssueReporter, matchType).visit(parseResult);
     }
   }
 
