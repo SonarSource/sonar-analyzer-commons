57e89ca24944efe3c80d9914b1964685f227abac
quentin-jaquier-sonarsource <quentin.jaquier@sonarsource.com>
2022-05-10 16:21:30 +0200

    Use "MatchType" to improve precision of S6002 finder

--- regex-parsing/src/test/java/org/sonarsource/analyzer/commons/regex/finders/FailingLookaheadFinderTest.java
+++ regex-parsing/src/test/java/org/sonarsource/analyzer/commons/regex/finders/FailingLookaheadFinderTest.java
@@ -20,20 +20,55 @@
 package org.sonarsource.analyzer.commons.regex.finders;
 
 import org.junit.jupiter.api.Test;
+import org.sonarsource.analyzer.commons.regex.MatchType;
 import org.sonarsource.analyzer.commons.regex.RegexIssueReporter;
 import org.sonarsource.analyzer.commons.regex.RegexParseResult;
 
 class FailingLookaheadFinderTest {
 
+  // The behavior should be the same for Partial, Both, Unknown and Not Supported.
   @Test
   void test() {
     Verifier.verify(new FailingLookaheadFinderCheck(), "FailingLookaheadFinder.yml");
+  }
+
+  @Test
+  void test_partial_match() {
+    Verifier.verify(new FailingLookaheadFinderCheckWithMatchType(MatchType.PARTIAL), "FailingLookaheadFinder.yml");
+  }
+
+  @Test
+  void test_unknown_match() {
+    Verifier.verify(new FailingLookaheadFinderCheckWithMatchType(MatchType.UNKNOWN), "FailingLookaheadFinder.yml");
+  }
+
+  @Test
+  void test_both_match() {
+    Verifier.verify(new FailingLookaheadFinderCheckWithMatchType(MatchType.BOTH), "FailingLookaheadFinder.yml");
+  }
+
+  @Test
+  void test_full_match() {
+    Verifier.verify(new FailingLookaheadFinderCheckWithMatchType(MatchType.FULL), "FailingLookaheadFinderFullMatch.yml");
   }
 
   static class FailingLookaheadFinderCheck extends FinderCheck {
     @Override
     public void checkRegex(RegexParseResult parseResult, RegexIssueReporter.ElementIssue regexElementIssueReporter, RegexIssueReporter.InvocationIssue invocationIssueReporter) {
       new FailingLookaheadFinder(regexElementIssueReporter, parseResult.getFinalState()).visit(parseResult);
+    }
+  }
+
+  static class FailingLookaheadFinderCheckWithMatchType extends FinderCheck {
+    private final MatchType matchType;
+
+    FailingLookaheadFinderCheckWithMatchType(MatchType matchType) {
+      this.matchType = matchType;
+    }
+
+    @Override
+    public void checkRegex(RegexParseResult parseResult, RegexIssueReporter.ElementIssue regexElementIssueReporter, RegexIssueReporter.InvocationIssue invocationIssueReporter) {
+      new FailingLookaheadFinder(regexElementIssueReporter, parseResult.getFinalState(), matchType).visit(parseResult);
     }
   }
 
