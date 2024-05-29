792d0fd67ab8578e0c418de46968030987c3b1ec
Anton Haubner <anton.haubner@sonarsource.com>
2024-05-29 09:18:05 +0200

    ACOMMONS-14: Move SonarLintCache into org.sonar.plugins
    The reason is that the possible package locations for extensions are
    restricted:
    https://github.com/SonarSource/sonarqube/blob/c16eb1bbf426b9b77a3fef3db64b61669a06259d/sonar-core/src/main/java/org/sonar/core/platform/PluginClassLoader.java#L51

--- commons/src/main/java/org/sonar/plugins/commons/SonarLintCache.java
+++ commons/src/main/java/org/sonar/plugins/commons/SonarLintCache.java
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-package org.sonarsource.analyzer.commons;
+package org.sonar.plugins.commons;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
