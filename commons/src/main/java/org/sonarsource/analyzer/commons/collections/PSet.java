0551c35b66c4cc97123c9e627d072f63dd1e2c4d
Solal Pirelli <solal.pirelli@sonarsource.com>
2024-07-09 11:55:31 +0200

    Add P{Map,Set,Stack}.of

--- commons/src/main/java/org/sonarsource/analyzer/commons/collections/PSet.java
+++ commons/src/main/java/org/sonarsource/analyzer/commons/collections/PSet.java
@@ -65,4 +65,224 @@ public interface PSet<E> extends Iterable<E> {
   default Stream<E> stream() {
     return StreamSupport.stream(this.spliterator(), false);
   }
+
+  /**
+   * Returns a persistent set containing zero elements.
+   *
+   * @param <E> the {@code PSet}'s element type
+   * @return an empty {@code PSet}
+   */
+  static <E> PSet<E> of() {
+    return PCollections.emptySet();
+  }
+
+  /**
+   * Returns a persistent set containing one element.
+   *
+   * @param <E> the {@code PSet}'s element type
+   * @param e1 the single element
+   * @return a {@code PSet} containing the specified element
+   */
+  static <E> PSet<E> of(E e1) {
+    return PCollections.<E>emptySet()
+      .add(e1);
+  }
+
+  /**
+   * Returns a persistent set containing two elements.
+   *
+   * @param <E> the {@code PSet}'s element type
+   * @param e1 the first element
+   * @param e2 the second element
+   * @return a {@code PSet} containing the specified elements
+   */
+  static <E> PSet<E> of(E e1, E e2) {
+    return PCollections.<E>emptySet()
+      .add(e1)
+      .add(e2);
+  }
+
+  /**
+   * Returns a persistent set containing three elements.
+   *
+   * @param <E> the {@code PSet}'s element type
+   * @param e1 the first element
+   * @param e2 the second element
+   * @param e3 the third element
+   * @return a {@code PSet} containing the specified elements
+   */
+  static <E> PSet<E> of(E e1, E e2, E e3) {
+    return PCollections.<E>emptySet()
+      .add(e1)
+      .add(e2)
+      .add(e3);
+  }
+
+  /**
+   * Returns a persistent set containing four elements.
+   *
+   * @param <E> the {@code PSet}'s element type
+   * @param e1 the first element
+   * @param e2 the second element
+   * @param e3 the third element
+   * @param e4 the fourth element
+   * @return a {@code PSet} containing the specified elements
+   */
+  static <E> PSet<E> of(E e1, E e2, E e3, E e4) {
+    return PCollections.<E>emptySet()
+      .add(e1)
+      .add(e2)
+      .add(e3)
+      .add(e4);
+  }
+
+  /**
+   * Returns a persistent set containing five elements.
+   *
+   * @param <E> the {@code PSet}'s element type
+   * @param e1 the first element
+   * @param e2 the second element
+   * @param e3 the third element
+   * @param e4 the fourth element
+   * @param e5 the fifth element
+   * @return a {@code PSet} containing the specified elements
+   */
+  static <E> PSet<E> of(E e1, E e2, E e3, E e4, E e5) {
+    return PCollections.<E>emptySet()
+      .add(e1)
+      .add(e2)
+      .add(e3)
+      .add(e4)
+      .add(e5);
+  }
+
+  /**
+   * Returns a persistent set containing six elements.
+   *
+   * @param <E> the {@code PSet}'s element type
+   * @param e1 the first element
+   * @param e2 the second element
+   * @param e3 the third element
+   * @param e4 the fourth element
+   * @param e5 the fifth element
+   * @param e6 the sixth element
+   * @return a {@code PSet} containing the specified elements
+   */
+  static <E> PSet<E> of(E e1, E e2, E e3, E e4, E e5, E e6) {
+    return PCollections.<E>emptySet()
+      .add(e1)
+      .add(e2)
+      .add(e3)
+      .add(e4)
+      .add(e5)
+      .add(e6);
+  }
+
+  /**
+   * Returns a persistent set containing seven elements.
+   *
+   * @param <E> the {@code PSet}'s element type
+   * @param e1 the first element
+   * @param e2 the second element
+   * @param e3 the third element
+   * @param e4 the fourth element
+   * @param e5 the fifth element
+   * @param e6 the sixth element
+   * @param e7 the seventh element
+   * @return a {@code PSet} containing the specified elements
+   */
+  static <E> PSet<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7) {
+    return PCollections.<E>emptySet()
+      .add(e1)
+      .add(e2)
+      .add(e3)
+      .add(e4)
+      .add(e5)
+      .add(e6)
+      .add(e7);
+  }
+
+  /**
+   * Returns a persistent set containing eight elements.
+   *
+   * @param <E> the {@code PSet}'s element type
+   * @param e1 the first element
+   * @param e2 the second element
+   * @param e3 the third element
+   * @param e4 the fourth element
+   * @param e5 the fifth element
+   * @param e6 the sixth element
+   * @param e7 the seventh element
+   * @param e8 the eighth element
+   * @return a {@code PSet} containing the specified elements
+   */
+  static <E> PSet<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8) {
+    return PCollections.<E>emptySet()
+      .add(e1)
+      .add(e2)
+      .add(e3)
+      .add(e4)
+      .add(e5)
+      .add(e6)
+      .add(e7)
+      .add(e8);
+  }
+
+  /**
+   * Returns a persistent set containing nine elements.
+   *
+   * @param <E> the {@code PSet}'s element type
+   * @param e1 the first element
+   * @param e2 the second element
+   * @param e3 the third element
+   * @param e4 the fourth element
+   * @param e5 the fifth element
+   * @param e6 the sixth element
+   * @param e7 the seventh element
+   * @param e8 the eighth element
+   * @param e9 the ninth element
+   * @return a {@code PSet} containing the specified elements
+   */
+  static <E> PSet<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8, E e9) {
+    return PCollections.<E>emptySet()
+      .add(e1)
+      .add(e2)
+      .add(e3)
+      .add(e4)
+      .add(e5)
+      .add(e6)
+      .add(e7)
+      .add(e8)
+      .add(e9);
+  }
+
+  /**
+   * Returns a persistent set containing ten elements.
+   *
+   * @param <E> the {@code PSet}'s element type
+   * @param e1 the first element
+   * @param e2 the second element
+   * @param e3 the third element
+   * @param e4 the fourth element
+   * @param e5 the fifth element
+   * @param e6 the sixth element
+   * @param e7 the seventh element
+   * @param e8 the eighth element
+   * @param e9 the ninth element
+   * @param e10 the tenth element
+   * @return a {@code PSet} containing the specified elements
+   */
+  static <E> PSet<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8, E e9, E e10) {
+    return PCollections.<E>emptySet()
+      .add(e1)
+      .add(e2)
+      .add(e3)
+      .add(e4)
+      .add(e5)
+      .add(e6)
+      .add(e7)
+      .add(e8)
+      .add(e9)
+      .add(e10);
+  }
 }
---
+++ commons/src/test/java/org/sonarsource/analyzer/commons/collections/PStackTest.java
@@ -0,0 +1,221 @@
+/*
+ * SonarSource Analyzers Commons
+ * Copyright (C) 2009-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc.1 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarsource.analyzer.commons.collections;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class PStackTest {
+  @Test
+  public void of0() {
+    assertThat(PStack.of()).isSameAs(PCollections.emptyStack());
+  }
+
+  @Test
+  public void of1() {
+    var stack = PStack.of(
+      "partridge in a pear tree"
+    );
+
+    assertThat(stack.size()).isEqualTo(1);
+    assertThat(stack.peek(0)).isEqualTo("partridge in a pear tree");
+  }
+
+  @Test
+  public void of2() {
+    var stack = PStack.of(
+      "partridge in a pear tree",
+      "turtle dove"
+    );
+
+    assertThat(stack.size()).isEqualTo(2);
+    assertThat(stack.peek(1)).isEqualTo("partridge in a pear tree");
+    assertThat(stack.peek(0)).isEqualTo("turtle dove");
+  }
+
+  @Test
+  public void of3() {
+    var stack = PStack.of(
+      "partridge in a pear tree",
+      "turtle dove",
+      "French hen"
+    );
+
+    assertThat(stack.size()).isEqualTo(3);
+    assertThat(stack.peek(2)).isEqualTo("partridge in a pear tree");
+    assertThat(stack.peek(1)).isEqualTo("turtle dove");
+    assertThat(stack.peek(0)).isEqualTo("French hen");
+  }
+
+  @Test
+  public void of4() {
+    var stack = PStack.of(
+      "partridge in a pear tree",
+      "turtle dove",
+      "French hen",
+      "calling bird"
+    );
+
+    assertThat(stack.size()).isEqualTo(4);
+    assertThat(stack.peek(3)).isEqualTo("partridge in a pear tree");
+    assertThat(stack.peek(2)).isEqualTo("turtle dove");
+    assertThat(stack.peek(1)).isEqualTo("French hen");
+    assertThat(stack.peek(0)).isEqualTo("calling bird");
+  }
+
+  @Test
+  public void of5() {
+    var stack = PStack.of(
+      "partridge in a pear tree",
+      "turtle dove",
+      "French hen",
+      "calling bird",
+      "gold ring"
+    );
+
+    assertThat(stack.size()).isEqualTo(5);
+    assertThat(stack.peek(4)).isEqualTo("partridge in a pear tree");
+    assertThat(stack.peek(3)).isEqualTo("turtle dove");
+    assertThat(stack.peek(2)).isEqualTo("French hen");
+    assertThat(stack.peek(1)).isEqualTo("calling bird");
+    assertThat(stack.peek(0)).isEqualTo("gold ring");
+  }
+
+  @Test
+  public void of6() {
+    var stack = PStack.of(
+      "partridge in a pear tree",
+      "turtle dove",
+      "French hen",
+      "calling bird",
+      "gold ring",
+      "geese a-laying"
+    );
+
+    assertThat(stack.size()).isEqualTo(6);
+    assertThat(stack.peek(5)).isEqualTo("partridge in a pear tree");
+    assertThat(stack.peek(4)).isEqualTo("turtle dove");
+    assertThat(stack.peek(3)).isEqualTo("French hen");
+    assertThat(stack.peek(2)).isEqualTo("calling bird");
+    assertThat(stack.peek(1)).isEqualTo("gold ring");
+    assertThat(stack.peek(0)).isEqualTo("geese a-laying");
+  }
+
+  @Test
+  public void of7() {
+    var stack = PStack.of(
+      "partridge in a pear tree",
+      "turtle dove",
+      "French hen",
+      "calling bird",
+      "gold ring",
+      "goose a-laying",
+      "swan a-swimming"
+    );
+
+    assertThat(stack.size()).isEqualTo(7);
+    assertThat(stack.peek(6)).isEqualTo("partridge in a pear tree");
+    assertThat(stack.peek(5)).isEqualTo("turtle dove");
+    assertThat(stack.peek(4)).isEqualTo("French hen");
+    assertThat(stack.peek(3)).isEqualTo("calling bird");
+    assertThat(stack.peek(2)).isEqualTo("gold ring");
+    assertThat(stack.peek(1)).isEqualTo("goose a-laying");
+    assertThat(stack.peek(0)).isEqualTo("swan a-swimming");
+  }
+
+  @Test
+  public void of8() {
+    var stack = PStack.of(
+      "partridge in a pear tree",
+      "turtle dove",
+      "French hen",
+      "calling bird",
+      "gold ring",
+      "goose a-laying",
+      "swan a-swimming",
+      "maid a-milking"
+    );
+
+    assertThat(stack.size()).isEqualTo(8);
+    assertThat(stack.peek(7)).isEqualTo("partridge in a pear tree");
+    assertThat(stack.peek(6)).isEqualTo("turtle dove");
+    assertThat(stack.peek(5)).isEqualTo("French hen");
+    assertThat(stack.peek(4)).isEqualTo("calling bird");
+    assertThat(stack.peek(3)).isEqualTo("gold ring");
+    assertThat(stack.peek(2)).isEqualTo("goose a-laying");
+    assertThat(stack.peek(1)).isEqualTo("swan a-swimming");
+    assertThat(stack.peek(0)).isEqualTo("maid a-milking");
+  }
+
+  @Test
+  public void of9() {
+    var stack = PStack.of(
+      "partridge in a pear tree",
+      "turtle dove",
+      "French hen",
+      "calling bird",
+      "gold ring",
+      "goose a-laying",
+      "swan a-swimming",
+      "maid a-milking",
+      "lady dancing"
+    );
+
+    assertThat(stack.size()).isEqualTo(9);
+    assertThat(stack.peek(8)).isEqualTo("partridge in a pear tree");
+    assertThat(stack.peek(7)).isEqualTo("turtle dove");
+    assertThat(stack.peek(6)).isEqualTo("French hen");
+    assertThat(stack.peek(5)).isEqualTo("calling bird");
+    assertThat(stack.peek(4)).isEqualTo("gold ring");
+    assertThat(stack.peek(3)).isEqualTo("goose a-laying");
+    assertThat(stack.peek(2)).isEqualTo("swan a-swimming");
+    assertThat(stack.peek(1)).isEqualTo("maid a-milking");
+    assertThat(stack.peek(0)).isEqualTo("lady dancing");
+  }
+
+  @Test
+  public void of10() {
+    var stack = PStack.of(
+      "partridge in a pear tree",
+      "turtle dove",
+      "French hen",
+      "calling bird",
+      "gold ring",
+      "goose a-laying",
+      "swan a-swimming",
+      "maid a-milking",
+      "lady dancing",
+      "lord a-leaping"
+    );
+
+    assertThat(stack.size()).isEqualTo(10);
+    assertThat(stack.peek(9)).isEqualTo("partridge in a pear tree");
+    assertThat(stack.peek(8)).isEqualTo("turtle dove");
+    assertThat(stack.peek(7)).isEqualTo("French hen");
+    assertThat(stack.peek(6)).isEqualTo("calling bird");
+    assertThat(stack.peek(5)).isEqualTo("gold ring");
+    assertThat(stack.peek(4)).isEqualTo("goose a-laying");
+    assertThat(stack.peek(3)).isEqualTo("swan a-swimming");
+    assertThat(stack.peek(2)).isEqualTo("maid a-milking");
+    assertThat(stack.peek(1)).isEqualTo("lady dancing");
+    assertThat(stack.peek(0)).isEqualTo("lord a-leaping");
+  }
+}
