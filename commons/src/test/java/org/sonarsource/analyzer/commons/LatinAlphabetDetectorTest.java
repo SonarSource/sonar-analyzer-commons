03046a2119c076944d1591aa43d84621994fa5db
Marcin Stachniuk <marcin.stachniuk@sonarsource.com>
2024-11-04 19:05:08 +0100

    Fix code smell

--- commons/src/test/java/org/sonarsource/analyzer/commons/LatinAlphabetDetectorTest.java
+++ commons/src/test/java/org/sonarsource/analyzer/commons/LatinAlphabetDetectorTest.java
@@ -20,7 +20,6 @@
 package org.sonarsource.analyzer.commons;
 
 import java.util.stream.Stream;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
